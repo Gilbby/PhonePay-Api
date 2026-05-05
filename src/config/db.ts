0462b9b63f6f22dea4e6205a70a3702988172dcb
Gibby <gilbertkomz@gmail>
2026-05-05 13:22:05 +0200

    Rate Limiter

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ const connectDB = async (): Promise<void> => {
     if (!uri) throw new Error('MONGODB_URI is not defined in environment variables');
 
     await mongoose.connect(uri);
-    console.log('✅ MongoDB connected successfully');
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error);
     process.exit(1);
